67091411c83bae4efdd5216545c392872e9636ca
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2012-01-24 00:11:12 +0100

    fixed handling of command-line arguments

--- src/gocept/package/doc.py
+++ src/gocept/package/doc.py
@@ -3,14 +3,11 @@
 
 import os
 import pkg_resources
-import sys
 
 
-def main(argv=sys.argv):
-    if not argv:
-        argv = ['-E']
+def main():
     sphinx_build = pkg_resources.load_entry_point(
         'Sphinx', 'console_scripts', 'sphinx-build')
     sphinx_build(
-        ['sphinx-build'] + argv +
+        ['sphinx-build', '-E'] +
         [os.getcwd() + x for x in ['/doc', '/build/doc']])
