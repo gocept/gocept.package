ad510b2d9e0a8bacba9a2a62db7a7564430f5cbc
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2012-02-15 16:24:29 +0100

    added tests for expanding the template, set up things inside a virtual Python due to the way buildout interacts with paster and hg

--- src/gocept/package/tests/test_skeleton.py
+++ src/gocept/package/tests/test_skeleton.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import datetime
+import gocept.package.doc
 import os
+import os.path
 import paste.script.command
 import shutil
 import tempfile
@@ -19,6 +22,35 @@ class Skeleton(unittest.TestCase):
         os.chdir(self.cwd)
         shutil.rmtree(self.tmpdir)
 
-    def test_expand_template(self):
-        paste.script.command.run(
-            'create -t gocept_package gocept.example'.split())
+    def expand_template(self):
+        try:
+            paste.script.command.run(
+                'create -t gocept_package gocept.example'.split() + [
+                    'description="An example package." ',
+                    'keywords="example package"',
+                    ])
+        except SystemExit:
+            pass
+
+    def content(self, rel_path):
+        return open(os.path.join(
+                self.tmpdir, 'gocept.example', *rel_path.split('/'))).read()
+
+    def test_expanding_template_creates_files(self):
+        self.expand_template()
+        self.assertEqual(
+            '', self.content('src/gocept/example/tests/__init__.py'))
+        self.assertIn(
+            str(datetime.date.today().year), self.content('COPYRIGHT.txt'))
+
+    def test_setup_py_is_functional(self):
+        # paster detects setup.py and creates egg-info from it
+        self.expand_template()
+        self.assertIn('Name: gocept.example\n',
+                      self.content('src/gocept.example.egg-info/PKG-INFO'))
+
+    def test_sphinx_docs_can_be_built(self):
+        self.expand_template()
+        os.chdir('gocept.example')
+        gocept.package.doc.main(['doc'])
+        self.assertIn('<html', self.content('build/doc/index.html'))
