fb7582de650cd7ddf83f9fba580cf4c572b0ec68
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2012-02-16 08:48:28 +0100

    reordered setup.py so that stuff changed most often appears near the top of the file

--- setup.py
+++ setup.py
@@ -9,69 +9,65 @@ import glob
 import os.path
 
 
-keywords = 'paste.script paster create template python package sphinx theme'
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
 
-install_requires = [
-    'PasteScript',
-    'distribute',
-    'pkginfo',
-    ]
 
-extras_require = {
-    'doc': [
-        'Sphinx>=1.0',
-        ],
-    'test': [
-        'gocept.testing',
-        'unittest2',
-        ],
-    }
+setup(
+    name='gocept.package',
+    version='1.0.dev0',
 
-entry_points = {
-    'console_scripts': [
-        'doc=gocept.package.doc:main',
+    install_requires=[
+        'PasteScript',
+        'distribute',
+        'pkginfo',
         ],
-    'paste.paster_create_template': [
-        'gocept_package = gocept.package.skeleton:Skeleton'
-        ],
-    }
 
-classifiers = """\
+    extras_require={
+        'doc': [
+            'Sphinx>=1.0',
+            ],
+        'test': [
+            'gocept.testing',
+            'unittest2',
+            ],
+        },
+
+    entry_points={
+        'console_scripts': [
+            'doc=gocept.package.doc:main',
+            ],
+        'paste.paster_create_template': [
+            'gocept_package = gocept.package.skeleton:Skeleton'
+            ],
+        },
+
+    author=('Thomas Lotze <tl at gocept dot com> and '
+            'Wolfgang Schnerring <ws at gocept dot com>'),
+    author_email="mail@gocept.com",
+    license="ZPL 2.1",
+    url="https://projects.gocept.com/projects/gocept-package/",
+
+    keywords='paste.script paster create template python package sphinx theme',
+    classifiers="""\
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 2 :: Only
-"""[:-1].split('\n')
-
-
-project_path = lambda *names: os.path.join(os.path.dirname(__file__), *names)
-
-longdesc = '\n\n'.join((open(project_path('README.txt')).read(),
-                        open(project_path('HACKING.txt')).read(),
-                        open(project_path('CHANGES.txt')).read()))
-
-data_files = [("", glob.glob(project_path("*.txt")))]
-
-
-setup(name='gocept.package',
-      version='1.0.dev0',
+"""[:-1].split('\n'),
       description=__doc__.strip(),
-      long_description=longdesc,
-      keywords=keywords,
-      author='Thomas Lotze <tl at gocept dot com> and Wolfgang Schnerring <ws at gocept dot com>',
-      author_email="mail@gocept.com",
-      url="https://projects.gocept.com/projects/gocept-package/",
-      license="ZPL 2.1",
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      data_files=data_files,
-      zip_safe=False,
-      namespace_packages=['gocept'],
-      install_requires=install_requires,
-      extras_require=extras_require,
-      entry_points=entry_points,
-      classifiers=classifiers,
-      )
+    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+            'README.txt',
+            'HACKING.txt',
+            'CHANGES.txt',
+            )),
+
+    namespace_packages=['gocept'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[("", glob.glob(project_path("*.txt")))],
+    zip_safe=False,
+    )
